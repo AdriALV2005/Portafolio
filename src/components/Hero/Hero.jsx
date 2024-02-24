bac7fd8999e6955583e62ed9a9cafb83a7a2d367
AdriALV2005 <adrimon7221@gmail.com>
2024-02-24 15:48:13 -0500

    finished hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Lupe</h1>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo dolor
+          labore magni laudantium totam voluptatibus! Autem voluptatibus
+          doloremque pariatur praesentium ut eos ad molestias quod, dolorem,
+          repudiandae ullam. Saepe, iste!
+        </p>
+        <a href="adrimon7221@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
